3dc91904995c837193df789cfa32ef75c8a06e60
nihalcshah <nihalcshah@gmail.com>
2022-06-18 16:00:58 -0400

    Itineraries

--- timeline/models.py
+++ timeline/models.py
@@ -5,14 +5,20 @@ from datetime import datetime
 class Itinerary(models.Model):
     title = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Itinerary'
+        verbose_name_plural = 'Itineraries'
+
     def __str__(self):
         return self.title
 
 class Event(models.Model):
     time = models.DateTimeField()
     place = models.CharField(max_length=500)
-    googlelink = models.CharField(max_length=1000)
-    description = models.CharField(max_length=600)
+    Travel = models.BooleanField(default=False, help_text="Check if this is a drive or flight")
+    hotel = models.BooleanField(default=False, help_text="Check if this is a hotel")
+    googlelink = models.CharField(max_length=1000, blank=True, help_text="Link to TripAdvisor or the Hotel (Optional)")
+    description = models.CharField(max_length=600, blank=True, help_text="Description of the event (Optional)")
     itin = models.ManyToManyField(Itinerary)
 
     formattedtime = " "
@@ -22,3 +28,6 @@ class Event(models.Model):
 class Date(models.Model):
     date = models.DateField()
     events = []
+
+    def __str__(self):
+        return self.date.strftime("%A, %B %d")
--- timeline/views.py
+++ timeline/views.py
@@ -5,47 +5,6 @@ from .models import Event, Itinerary, Date
 
 
 # Create your views here.
-def Home(request):
-    events = Event.objects.all()
-    for i in events:
-        i.time = i.time.strftime("%A, %B %d|%H:%M").split("|")
-        i.formattedtime = i.time[1]
-        ind = i.description.rfind(".")
-        if ind != -1:
-            i.description = i.description[:ind+1]
-
-    if request.method == 'POST':
-        dates = {i.time[0] for i in events}
-        organizebydate = {date:[] for date in dates}
-        for i in range(len(events)):
-            organizebydate[events[i].time[0]].append((events[i], i%2))
-        #sort by time[1]
-        for i in organizebydate:
-            organizebydate[i] = sorted(organizebydate[i], key=lambda x: x[0].time[1])
-        print(organizebydate)
-        selection = request.POST.get('date')
-        print(selection)
-        if selection=="default":
-            return render(request, 'index.html', context={
-                "dates": dates,
-
-                "chosen":False,
-            })
-        
-        return render(request, 'index.html', context={
-                "dates": dates,
-                "timelinevals": organizebydate[selection],
-                "chosen":True,
-                "date":selection,
-        })
-    else:
-        dates = {i.time[0] for i in events}
-
-        return render(request, 'index.html', context={
-            "dates": dates,
-            "chosen":False,
-        })
-
 # def Home(request):
 #     events = Event.objects.all()
 #     for i in events:
@@ -54,16 +13,102 @@ def Home(request):
 #         ind = i.description.rfind(".")
 #         if ind != -1:
 #             i.description = i.description[:ind+1]
+
 #     if request.method == 'POST':
-#         #Sort into itineraries
-#         itins = {i.title:[] for i in Itinerary.objects.all()}
-#         for i in events:
-#             itins[i.itin.title].append(i)
+#         dates = {i.time[0] for i in events}
+#         organizebydate = {date:[] for date in dates}
+#         for i in range(len(events)):
+#             organizebydate[events[i].time[0]].append((events[i], i%2))
+#         #sort by time[1]
+#         for i in organizebydate:
+#             organizebydate[i] = sorted(organizebydate[i], key=lambda x: x[0].time[1])
+#         print(organizebydate)
+#         selection = request.POST.get('date')
+#         print(selection)
+#         if selection=="default":
+#             return render(request, 'index.html', context={
+#                 "dates": dates,
+
+#                 "chosen":False,
+#             })
         
-#     return render(request, 'index.html', context={
-#             "dates": [],
+#         return render(request, 'index.html', context={
+#                 "dates": dates,
+#                 "timelinevals": organizebydate[selection],
+#                 "chosen":True,
+#                 "date":selection,
+#         })
+#     else:
+#         dates = {i.time[0] for i in events}
+
+#         return render(request, 'index.html', context={
+#             "dates": dates,
 #             "chosen":False,
 #         })
+
+def Home(request):
+    events = Event.objects.all()
+    # for i in events:
+    #     i.time = i.time.strftime("%A, %B %d|%H:%M").split("|")
+    #     i.formattedtime = i.time[1]
+    #     ind = i.description.rfind(".")
+    #     if ind != -1:
+    #         i.description = i.description[:ind+1]
+    if request.method == 'POST':
+        #Sort into itineraries
+        print(Itinerary.objects.all())
+        print(events)
+        
+        itins = {i.title:[] for i in Itinerary.objects.all()}
+        # print(itins)
+        for i in events:
+            if len(i.description)>500:
+                ind = i.description.rfind(".")
+                if ind != -1:
+                    i.description = i.description[:ind+1]
+            # print(i.itin.all())
+            for j in i.itin.all():
+                itins[j.title].append(i)
+        selection = request.POST.get('Itinerary')
+        #create date dicts
+        chosenitin = itins[selection]
+        dates = {i.time.date() for i in chosenitin}
+        organizebydate = {date:[] for date in dates}
+        for ev in chosenitin:
+            organizebydate[ev.time.date()].append(ev)
+            ev.time = ev.time.strftime("%A, %B %d|%H:%M").split("|")[1]
+        print(organizebydate)
+        #sort by .time.time()
+        dates = []
+        count = 0
+        for i in organizebydate:
+            organizebydate[i] = sorted(organizebydate[i], key=lambda x: x.time)
+            k = []
+            for ev in organizebydate[i]:
+                k.append((ev, count%2))
+                print(ev.time)
+                count += 1
+            organizebydate[i] = k
+            d = Date(date=i)
+            d.events = organizebydate[i]
+            d.save()
+            dates.append(d)
+        #sort dates by date
+        dates = sorted(dates, key=lambda x: x.date)
+        print(dates)
+        itins = {i for i in Itinerary.objects.all()}
+        return render(request, 'index.html', context={
+            "Itineraries" : itins,
+            "Itinerary" : selection,
+            "dates": dates,
+            "chosen":True,
+        })
+    else:
+        itins = {i.title for i in Itinerary.objects.all()}
+        return render(request, 'index.html', context={
+            "Itineraries": itins,
+            "chosen":False,
+        })
             
             
         
